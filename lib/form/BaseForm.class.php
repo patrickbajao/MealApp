a29ab701f0faa31947762206b10bbb661c8ae4d1
Patrick Bajao <earl.bajao@gmail.com>
2010-09-03 09:55:54 +0800

    generated the MealApp project using propel as the ORM and also generated the frontend app.

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    MealApp
+ * @subpackage form
+ * @author     Your name here 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
