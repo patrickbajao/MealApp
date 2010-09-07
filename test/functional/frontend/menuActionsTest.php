0d0cb68393d04ea78f75ab39cd9aa688a5ade29f
Patrick Bajao <earl.bajao@gmail.com>
2010-09-07 09:36:27 +0800

    Added a route (/chosen/menu) that points to the show action of menu module. Also created a new test for the story 'As a user, I can pick a menu item from the place chosen to eat' under menuActionsTest.php.

--- test/functional/frontend/menuActionsTest.php
+++ test/functional/frontend/menuActionsTest.php
@@ -5,20 +5,20 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 $browser = new sfTestFunctional(new sfBrowser());
 
 /**
- * Feature 1: As a user, I can login to my account
+ * Feature 1: As a user, I can pick a menu item from the place chosen to eat
  */
 
-$browser->info('1 - User Login')->
-    info('1.1 - User enters his correct username and password then clicks the Login button')->
-    get('/')->
-    with('response')->
+$browser->info('1 - Pick a menu item from the place chosen to eat')->
+    info('1.1 - User picks a menu item')->
+    get('/chosen/menu')->
+    click('Yum', array(), array('position' => 1))->
     with('request')->begin()->
-        click('Login', array('signin' => array('username' => 'tester', 'password' => 'p4ssw0rd!')))->
+        click('Place Order')->
     end()->
     with('response')->
         isRedirected()->
         followRedirect()->
     info('1.1.1 - Application redirects user to his home page')->
     with('response')->begin()->
-        checkElement('#welcome', true)->
+        checkElement('.info', '/Your order has been placed./i')->
     end();
