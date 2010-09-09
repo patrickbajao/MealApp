1c6a93d70869ee9d5b210396e8728523f79a751c
Patrick Bajao <earl.bajao@gmail.com>
2010-09-09 13:31:04 +0800

    Added comments on the unit tests.

--- test/unit/model/MealPeerTest.php
+++ test/unit/model/MealPeerTest.php
@@ -5,8 +5,8 @@ include(dirname(__FILE__).'/../../bootstrap/Propel.php');
 $t = new lime_test(1);
 
 $t->comment('::getMealPlaceId()');
-$place = PlacePeer::doSelectOne(new Criteria());
-$c = new Criteria();
+$place = PlacePeer::doSelectOne(new Criteria()); //Select the first row in the list of places
+$c     = new Criteria();
 $c->add(MealPeer::PLACE_ID, $place->getId(), Criteria::EQUAL);
-$meal = MealPeer::doSelectOne($c);
+$meal = MealPeer::doSelectOne($c); //Select the row of specific meal given a specific place_id
 $t->is(MealPeer::getMealPlaceId($place->getId()), $meal->getPlaceId(), '::getMealPlaceId() returns the place_id of a specific meal');
--- test/unit/model/MenuPeerTest.php
+++ test/unit/model/MenuPeerTest.php
@@ -5,7 +5,7 @@ include(dirname(__FILE__).'/../../bootstrap/Propel.php');
 $t = new lime_test(1);
 
 $t->comment('::getPlaceMenu()');
-$place = PlacePeer::doSelectOne(new Criteria);
-$c = new Criteria();
+$place = PlacePeer::doSelectOne(new Criteria); //Select the first row in the list of places
+$c     = new Criteria();
 $c->add(MenuPeer::PLACE_ID, $place->getId(), Criteria::EQUAL);
 $t->is(MenuPeer::getPlaceMenu($place->getId()), MenuPeer::doSelectOne($c), '::getPlaceMenu() returns the menu of a specific meal');
