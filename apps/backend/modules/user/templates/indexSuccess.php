c88c840e9749a39ba7ee31ee54925a75a55ad895
Patrick Bajao <earl.bajao@gmail.com>
2010-09-16 07:13:14 +0800

    secured backend app

---
+++ apps/backend/modules/user/templates/indexSuccess.php
@@ -0,0 +1,2 @@
+<div class="title">Home</div>
+<h1 id="welcome">Welcome <?php echo $username?>!</h1>
--- apps/backend/templates/login.php
+++ apps/backend/templates/login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <?php include_http_metas() ?>
@@ -13,18 +13,9 @@
         <div id="container">
             <div id="header">
                 <h1 id="logo"><?php echo link_to('<span>LunchApp</span>', '@homepage') ?></h1>
-                <?php if($sf_user->isAuthenticated()): ?>
-                <ul id="navbar">
-                    <li><?php echo link_to('Home', '@homepage') ?></li>
-                    <li><?php echo link_to('Places', '@place') ?></li>
-                    <li><?php echo link_to('Users', '@sf_guard_user') ?></li>
-                    <li><?php echo link_to('Logout', '@sf_guard_signout') ?></li>
-                </ul>
-                <?php endif; ?>
             </div>
             <div id="content">
                 <?php echo $sf_content ?>
-                <div class="clear"></div>
             </div>
         </div>
     </div>
