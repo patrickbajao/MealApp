46ec3aaf1ecd89127901738c437eb9ced8bfad89
Patrick Bajao <earl.bajao@gmail.com>
2010-09-16 09:26:15 +0800

    generated meal module for backend app

---
+++ apps/backend/modules/meal/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/mealGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/mealGeneratorHelper.class.php';
+
+/**
+ * meal actions.
+ *
+ * @package    MealApp
+ * @subpackage meal
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class mealActions extends autoMealActions
+{
+}
