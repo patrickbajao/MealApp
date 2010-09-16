72029e8797cbb2d23ad9f5d9f03868db782e6868
Patrick Bajao <earl.bajao@gmail.com>
2010-09-16 09:22:34 +0800

    modified templates of item module

--- apps/backend/modules/item/templates/editSuccess.php
+++ apps/backend/modules/item/templates/editSuccess.php
@@ -1,20 +1,18 @@
 <?php use_helper('I18N', 'Date') ?>
 <?php include_partial('item/assets') ?>
-
+<div class="title"><?php echo __('Edit Item', array(), 'messages') ?></div>
 <div id="sf_admin_container">
-  <h1><?php echo __('Edit Item', array(), 'messages') ?></h1>
+    <?php include_partial('item/flashes') ?>
 
-  <?php include_partial('item/flashes') ?>
+    <div id="sf_admin_header">
+        <?php include_partial('item/form_header', array('item' => $item, 'form' => $form, 'configuration' => $configuration)) ?>
+    </div>
 
-  <div id="sf_admin_header">
-    <?php include_partial('item/form_header', array('item' => $item, 'form' => $form, 'configuration' => $configuration)) ?>
-  </div>
+    <div id="sf_admin_content">
+        <?php include_partial('item/form', array('item' => $item, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
+    </div>
 
-  <div id="sf_admin_content">
-    <?php include_partial('item/form', array('item' => $item, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
-  </div>
-
-  <div id="sf_admin_footer">
-    <?php include_partial('item/form_footer', array('item' => $item, 'form' => $form, 'configuration' => $configuration)) ?>
-  </div>
+    <div id="sf_admin_footer">
+        <?php include_partial('item/form_footer', array('item' => $item, 'form' => $form, 'configuration' => $configuration)) ?>
+    </div>
 </div>
