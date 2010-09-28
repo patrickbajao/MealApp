61cecdf1451e524a07f77e1ac51c3f1e76239d29
Patrick Bajao <earl.bajao@gmail.com>
2010-09-28 20:59:34 +0800

    added link to meals page for viewing past and future meals

--- apps/frontend/modules/meal/templates/indexSuccess.php
+++ apps/frontend/modules/meal/templates/indexSuccess.php
@@ -6,3 +6,7 @@
     <?php endif; ?>
     <?php include_partial('meal/scheduled_meals', array('meals' => $meals, 'user' => $sf_user->getGuardUser())) ?>
 </div>
+<div class="other-meals">
+    <?php echo link_to('<span><< Past Meals</span>', '@meal_list?tense=past', array('class' => 'past')) ?>
+    <?php echo link_to('<span>Future Meals >></span>', '@meal_list?tense=future', array('class' => 'future')) ?>
+</div>
