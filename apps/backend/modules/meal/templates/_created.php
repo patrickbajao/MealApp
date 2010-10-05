a644609850abc513f9a1a0c19b5d09029fb169df
Patrick Bajao <earl.bajao@gmail.com>
2010-10-06 02:25:26 +0800

    added _created.php and _updated.php partials to display the created_at and updated_at fields in the meal edit form

---
+++ apps/backend/modules/meal/templates/_created.php
@@ -0,0 +1,4 @@
+<div class="sf_admin_form_row">
+  <label>Created At</label>
+  <?php echo date('F j, Y g:i a', strtotime($form->getObject()->getCreatedAt())) ?>
+</div>
---
+++ apps/backend/modules/meal/templates/_updated.php
@@ -0,0 +1,4 @@
+<div class="sf_admin_form_row">
+  <label>Updated At</label>
+  <?php echo date('F j, Y g:i a', strtotime($form->getObject()->getUpdatedAt())) ?>
+</div>
