ca8ab2374fe99e4014cab843ea67eb91e510537a
Patrick Bajao <earl.bajao@gmail.com>
2010-10-07 02:50:51 +0800

    removed functions that are not needed

--- lib/test/MealAppTestFunctional.class.php
+++ lib/test/MealAppTestFunctional.class.php
@@ -12,20 +12,5 @@ class MealAppTestFunctional extends sfTestFunctional
     public function login($username, $password) {
         return $this->post('/sfGuardAuth/signin', array('signin' => array('username' => $username, 'password' => $password, 'remember' => false, '_csrf_token' => true)));
     }
-    
-    public function getMealWithChosenPlace() {
-        $c = new Criteria();
-        $c->add(MealPeer::VOTING_STOPPED, 1);
-        $c->add(MealPeer::ORDERING_STOPPED, 0);
-        $meal  = MealPeer::doSelectOne($c);
-        return $this;
-    }
-    
-    public function getItemFromPlace($place_id) {
-        $c = new Criteria();
-        $c->add(ItemPeer::PLACE_ID, $place_id);
-        $item = ItemPeer::doSelectOne($c);
-        return $this;
-    }
-    
+        
 }
